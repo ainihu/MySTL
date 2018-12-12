c2263f4d7161b9f14096d3da3d43c495c4600a2d
ainihu <1448122879@qq.com>
2018-12-12 22:05:17 +0800

    写了不少的bug

--- MyVector.hpp
+++ MyVector.hpp
@@ -9,7 +9,7 @@
  * operator[]:和at一样
  * front:取出第一个元素
  * back:取出最后一个元素
- * data:返回指向vector中第一个数据的指针,或空vector之后的位置
+ * _data:返回指向vector中第一个数据的指针,或空vector之后的位置
  * begin \ cbegin
  * end \ cend
  * rbegin \ crbegin
@@ -44,51 +44,304 @@
 #define MYSTL_MYVECTOR_H
 
 #include <iostream>
+#include <ostream>
+#include <stdexcept>
+
+using std::ostream;
+using std::out_of_range;
+using std::domain_error; // 执行一段程序的先决条件不满足
 
 template <typename T>
 class MyVector {
 private:
-    int capacity; // 向量的容量
-    int size; // 数据的个数
-    T * data; // 数据指针
+    int _capacity; // 向量的容量
+    int _size; // 数据的个数
+    T *_data; // 数据指针
+
 public:
-
 // 特殊函数
-    // 有参构造函数,用explicit修饰表示不使用隐式类型转换
-    MyVector(int = 0){
-        if(0 >= size)
-        {
-            this->capacity = 0;
-            this->size = 0;
-            this->data = NULL;
-        }
-        else
-        {
-            try {
-
-            }
-            catch (...)
-            {
-                throw ;
-            }
+    // 有参构造函数 用explicit修饰表示不使用隐式类型转换
+    MyVector(int size = 0) {
+        if (0 >= size) {
+            this->_capacity = 0;
+            this->_size = 0;
+            this->_data = NULL;
+        } else {
+            this->_data = new T[size];
+            if (NULL == this->_data) // 如果申请内存失败
+                size = 0;
+            this->_capacity = size;
+            this->_size = size;
         }
     }
 
     // 有参构造函数
-    MyVector(int, MyVector &){
-
+    MyVector(int size, T v) {
+        if (0 >= size) {
+            this->_capacity = 0;
+            this->_size = 0;
+            this->_data = NULL;
+        } else {
+            this->_data = new T[size];
+            if (NULL == this->_data)
+                size = 0;
+            this->_size = size;
+            this->_capacity = size;
+            for (int i = 0; i < size; ++i) {
+                this->_data[i] = v;
+            }
+        }
     }
 
     // 赋值拷贝构造函数
-    MyVector(const MyVector & v){
+    MyVector(const MyVector &v) {
+        if (0 == v._capacity) {
+            this->_capacity = 0;
+            this->_size = 0;
+            this->_data = NULL;
+            return;
+        } else {
+            int size = 0;
+            this->_data = new T[v._capacity];
+            if (NULL == this->_data) {
+                this->_capacity = 0;
+                this->_size = 0;
+                return;
+            }
+            // 初始化,分配内存,拷贝数据
+            this->_size = v._size;
+            this->_capacity = v._capacity;
+            for (int i = 0; i < v._size; ++i) {
+                this->_data[i] = v._data[i];
+            }
+        }
 
     }
+
     //析构函数
-    ~MyVector(){
+    ~MyVector() {
+        if (NULL == this->_data)
+            delete[]this->_data;
+        this->_data = NULL;
+        this->_size = 0;
+        this->_capacity = 0;
+    }
 
+    // operator <<, 链式编程
+    friend ostream &operator<<(ostream &out, MyVector &v) {
+        for (int i = 0; i < v._size; ++i) {
+            out << v[i] << " ";
+        }
+        return out;
+    }
+
+// 成员函数
+    // operator=赋值语句
+    MyVector &operator=(MyVector &v) {
+        if (&v == this) // 如果是自己给自己赋值,直接return
+            return *this;
+
+        if (v._data != NULL) // 释放原来的内存
+            delete[]v._data;
+
+        // 初始化,分配内存
+        v._data = new T[this->_capacity];
+        if (NULL == v._data) {// 申请内存失败
+            v._size = 0;
+            v._capacity = 0;
+            return *this;
+        }
+        v._size = this->_size;
+        v._capacity = this->_capacity;
+
+
+        // 拷贝数据
+        for (int i = 0; i < this->_size; ++i) {
+            v._data[i] = this->_data[i];
+        }
+        return *this;
+    }
+
+    // 填充
+    void assign(int size, T v) {
+        int resize = size;
+
+        // 内存空间不够重新配置
+        if (this->_capacity < size) {
+            resize *= 2;
+            delete[]this->_data;
+            this->_data = new T[resize];
+            if (NULL == this->_data) {
+                resize /= 2;
+                this->_data = new T[resize];
+            }
+            // 初始化
+            this->_capacity = resize;
+            this->_size = size;
+        }
+        // 赋值
+        for (int i = 0; i < size; ++i) {
+            this->_data[i] = v;
+        }
     }
 
 
+// 元素存取
+    // at 函数
+    T &at(int index) {
+        if (index >= this->_size || index < 0)
+            throw out_of_range("out of range");
+        return this->_data[index];
+    }
+
+    // operator[]
+    T &operator[](int index) {
+        return this->at(index);
+    }
+
+    // 获取front
+    T &front() {
+        if (this->_size == 0) {
+            throw out_of_range("size is zero");
+        } else {
+            return this->_data[0];
+        }
+    }
+
+    // 获取back
+    T &back() {
+        if (this->_size == 0) {
+            throw out_of_range("size is zero");
+        } else {
+            return this->_data[this->_size - 1];
+        }
+    }
+
+    // 获取data指针, C++
+    T *data() const {
+        return this->_data;
+    }
+
+// 容量
+
+    // 是否为空
+    bool empty() const {
+        return this->_size == 0;
+    }
+
+    // 获取size
+    int size() const {
+        return this->_size;
+    }
+
+    // 重新配置大小
+    void reserve(int size) {
+        if (this->_size < size) {
+            T *tmp = new T[size];
+            if (NULL == tmp)
+                return;
+            // 复制原来的数据
+            for (int i = 0; i < this->_size; ++i) {
+                tmp[i] = this->_data[i];
+            }
+
+            delete[]this->_data; // 释放原来的内存
+            this->_data = tmp;
+            this->_capacity = size;
+        }
+    }
+
+    // 获取容量
+    int capacity() const {
+        return this->_capacity;
+    }
+
+    // 释放不必要内存
+    void shrink_to_fit() {
+        if (this->_size < this->_capacity) {
+            T *tmp = new T[this->_size];
+            if (NULL == tmp)
+                return;
+            for (int i = 0; i < this->_size; ++i) {
+                tmp[i] = this->_data[i];
+            }
+
+            delete[]this->_data; // 释放原来的内存
+            this->_data = tmp;
+            this->_capacity = this->_size;
+        }
+    }
+
+// 修改器
+
+    // 清空容器,懒
+    void clear() {
+        this->_size = 0;
+    }
+
+    // insert
+    void insert(int pos, T value) {
+        if (pos < 0 || pos > this->_size)
+            return;
+        if ((this->_size + 1) > this->_capacity)
+            this->reserve(this->_size * 2); // 二倍内存
+        for (int i = this->_size; i >= pos; --i) {
+            if (i == pos) {
+                this->_data[i] = value;
+            } else {
+                this->_data[i] = this->_data[i - 1];
+            }
+        }
+        this->_size++;
+    }
+
+    // erase
+    T erase(int pos) {
+        if (pos < 0 || pos >= this->_size)
+            throw out_of_range("out of range");
+        for (int i = 0; i < this->_size - 1; ++i) {
+            if (i >= pos)
+                this->_data[i] = this->_data[i + 1];
+        }
+        --this->_size;
+    }
+
+    // push_back
+    void push_back(T value) {
+        if ((this->_size + 1) > this->_capacity)
+            this->reserve(this->_size * 2);
+        this->_data[this->_size] = value;
+        this->_size++;
+    }
+
+    // pop_back
+    void pop_back() {
+        if (this->_size == 0)
+            throw out_of_range("out of range");
+        this->_size--;
+    }
+
+    // resize
+    void resize(int size){
+        if(this->_size >= size)
+            this->_size = size;
+        else
+            throw domain_error("size must less than _size");
+    }
+
+    void resize(int size, T value){
+        if (this->_size > size)
+            this->_size = size;
+        else{
+            if(size > this->_capacity)
+                this->reserve(size * 2);
+            // 填充值
+            for (int i = this->_size; i < size; ++i) {
+                    this->_data[i] = value;
+            }
+            this->_size = size;
+        }
+    }
 };
 
 
--- main.cpp
+++ main.cpp
@@ -9,6 +9,43 @@ using std::vector;
 
 int main() {
     MyVector<int> s;
-    cout << "hello world" << endl;
+    MyVector<int> s1(2, 3);
+    auto s2 = s1;
+
+    cout << "operator=:" << s2 << endl;
+    s2.assign(3, 4);
+    cout << "assign:" << s2 << endl;
+    cout << "at:" << s2.at(1) << endl;
+    cout << "operator[]:" << s2[1] << endl;
+    cout << "front:" << s2.front() << endl;
+    cout << "back:" << s2.back() << endl;
+    cout << "data:" << *(s2.data() + 1) << endl;
+    cout << "empty:" << s2.empty() << endl;
+    cout << "size:" << s2.size() << endl;
+    s2.reserve(7);
+    s2.reserve(1);
+    cout << "reserve:" << s2.capacity() << endl;
+    s2.shrink_to_fit();
+    cout << "shrink_to_fit:" << s2.capacity() << endl;
+    cout << "s2:" << s2 << endl;
+    s2.clear();
+    cout << "clear:" << s2 << endl;
+    s2.insert(0, 3);
+    s2.insert(1, 2);
+    s2.insert(2, 1);
+    cout << "insert s2:" << s2 << endl;
+    s2.erase(1);
+    cout << "erase:" << s2 << endl;
+    s2.push_back(9);
+    cout << "push_back:" << s2 << endl;
+    s2.insert(2, 1);
+    cout << "****s2.insert(2, 1);" << endl;
+    s2.pop_back();
+    cout << "pop_back:" << s2 << endl;
+    s2.resize(2);
+    cout << "resize(int)" << s2 << endl;
+    s2.shrink_to_fit();
+    s2.resize(4, 2);
+    cout << "resize(int, T)" << s2 << endl;
     return 0;
 }
